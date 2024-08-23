8e551d63c09606abf197adea01fa9c179e5f9a65
pb4git <46708399+pb4git@users.noreply.github.com>
2024-08-23 17:45:30 +0200

    apt install flameshot

--- run_onchange_install-stuff.sh
+++ run_onchange_install-stuff.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install ripgrep cifs-utils samba remmina filelight syncthing micro gnome-tweaks borgbackup lm-sensors exiftool qbittorrent syncthing neovim htop stress s-tui nload
+sudo apt install ripgrep cifs-utils samba remmina filelight syncthing micro gnome-tweaks borgbackup lm-sensors exiftool qbittorrent syncthing neovim htop stress s-tui nload flameshot
 
 ############################
 # OBSIDIAN LATEST
