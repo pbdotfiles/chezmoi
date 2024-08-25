2c80d2ac273a0c8248ca02c2bc073f06471294f9
pb4git <pb.pubpub@gmail.com>
2024-08-25 11:06:17 +0200

    disable fonts download

--- run_onchange_install-stuff.sh
+++ run_onchange_install-stuff.sh
@@ -77,31 +77,31 @@ sudo apt update
 
 ############################
 # FONTS
-fonts_dir="${HOME}/.local/share/fonts"
-if [ ! -d "${fonts_dir}" ]; then
-    echo "mkdir -p $fonts_dir"
-    mkdir -p "${fonts_dir}"
-else
-    echo "Found fonts dir $fonts_dir"
-fi
+#fonts_dir="${HOME}/.local/share/fonts"
+#if [ ! -d "${fonts_dir}" ]; then
+    #echo "mkdir -p $fonts_dir"
+    #mkdir -p "${fonts_dir}"
+#else
+    #echo "Found fonts dir $fonts_dir"
+#fi
 
 # FIRA CODE
-version=5.2
-zip=Fira_Code_v${version}.zip
-curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
-unzip -o -q -d ${fonts_dir} ${zip}
-rm ${zip}
+#version=5.2
+#zip=Fira_Code_v${version}.zip
+#curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
+#unzip -o -q -d ${fonts_dir} ${zip}
+#rm ${zip}
 
 # NERD FONTS
-wget -q -O - https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'zip"$' | awk -F'"' ' {print $4} ' | while read -r url; do
-	echo "downloading: $url"
-        wget -q -O font.zip "$url"
-        unzip -o -q -d ${fonts_dir} font.zip
-        rm font.zip
-done
+#wget -q -O - https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'zip"$' | awk -F'"' ' {print $4} ' | while read -r url; do
+	#echo "downloading: $url"
+        #wget -q -O font.zip "$url"
+        #unzip -o -q -d ${fonts_dir} font.zip
+        #rm font.zip
+#done
 
 # install the fonts
-fc-cache -f
+#fc-cache -f
 ##############################
 # PYCHARM 2024.2
 wget -q -O pycharm.tar.gz https://download.jetbrains.com/python/pycharm-community-2024.2.tar.gz
