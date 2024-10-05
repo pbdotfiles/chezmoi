e0ca936e68108254fa6f58bdbc84ba64868eb566
pb4git <pb.pubpub@gmail.com>
2024-10-05 13:36:22 +0200

    temporarily disable nvidia driver install

--- run_onchange_21_nvidia_drivers.sh
+++ run_onchange_21_nvidia_drivers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # PPA: https://launchpad.net/~graphics-drivers/+archive/ubuntu/ppa
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
-sudo apt install nvidia-driver-555 nvtop -y
+# sudo add-apt-repository ppa:graphics-drivers/ppa
+# sudo apt update
+# sudo apt install nvidia-driver-555 nvtop -y
