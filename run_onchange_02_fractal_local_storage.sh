7befebfa97c5c9db957ea4dcc6957d840acd866f
pb4git <pb.pubpub@gmail.com>
2024-10-05 09:16:50 +0200

    various improvements following fractal22 setup

--- run_onchange_02_fractal_local_storage.sh
+++ run_onchange_02_fractal_local_storage.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 
-
-{{- if eq .chezmoi.hostname "fractal22" }}
-
+# Unmount all filesystems
 sudo umount -a
 
+# Create /mnt/ext4_data directory if it doesn't exist
 if [ ! -d /mnt/ext4_data ]; then
-	sudo mkdir /mnt/ext4_data
+    sudo mkdir /mnt/ext4_data
 fi
+
+# Remove immutable attribute if set
 sudo chattr -i /mnt/ext4_data
+
+# Change ownership to user 'paul'
 sudo chown -R paul /mnt/ext4_data
+
+# Set immutable attribute
 sudo chattr +i /mnt/ext4_data
 
-# FSTAB
+# Remove existing fstab entry for the specified UUID
 temp_file="/tmp/fstab_temp"
 grep -v "bca615bc" /etc/fstab > "$temp_file"
 sudo mv "$temp_file" /etc/fstab
 
-# Append this at the end of /etc/fstab :
+# Append the new fstab entry if it doesn't already exist (it shouldn't).
 # use "fdisk -l" and "blkid" to get the UUID
 sudo tee -a /etc/fstab >/dev/null <<EOF
 UUID="bca615bc-2c63-4db8-8408-ddd48b27ab55" /mnt/ext4_data ext4 defaults 0 2
 EOF
 
+# Mount all filesystems
 sudo mount -a
 
-rmdir ~/Downloads ~/Documents ~/Pictures
-ln -s /mnt/ext4_data/Downloads ~/Downloads
-ln -s /mnt/ext4_data/Documents ~/Documents
-ln -s /mnt/ext4_data/Pictures ~/Pictures
-ln -s /mnt/ext4_data/Torrents ~/Torrents
+# Remove existing directories and create symbolic links if they don't already exist
+for dir in Downloads Documents Pictures Torrents; do
+    if [ -L ~/"$dir" ]; then
+	rm ~/"$dir"
+    fi
+    if [ -d ~/"$dir" ]; then
+        rmdir ~/"$dir"
+    fi
+    ln -s /mnt/ext4_data/"$dir" ~/"$dir"
+done
 
-{{- end }}
