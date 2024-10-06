b3be8dd5249e39ae141fa79c787a7de1d80edd3d
pb4git <pb.pubpub@gmail.com>
2024-10-06 22:46:55 +0200

    freeze wine-staging to 9.17 version. Apply winetricks dxvk and isolate_home

--- run_onchange_20_mint_setup.sh
+++ run_onchange_20_mint_setup.sh
@@ -33,8 +33,17 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
 sudo apt update
-sudo apt install --yes --install-recommends winehq-staging
+# The line above installs wine's latest staging version. It doesn't work with Trackmania.
+# sudo apt install --yes --install-recommends winehq-staging # FIXEDVERSION
+# Instead we force install the latest version known to work with Trackmania:
+sudo apt install --install-recommends wine-staging-i386=9.17~noble-1 -y
+sudo apt install --install-recommends wine-staging-amd64=9.17~noble-1 -y
+sudo apt install --install-recommends wine-staging=9.17~noble-1 -y
+sudo apt install --install-recommends winehq-staging=9.17~noble-1 -y
+
 sudo apt install winetricks -y
+winetricks dxvk
+winetricks isolate_home
 
 ##############################
 # PYCHARM 2024.2
