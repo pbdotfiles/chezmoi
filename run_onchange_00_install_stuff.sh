7b50799121d8dfc7efce30be682efb8eb05bdb26
pb4git <pb.pubpub@gmail.com>
2024-10-07 20:14:34 +0200

    apt install sshfs

--- run_onchange_00_install_stuff.sh
+++ run_onchange_00_install_stuff.sh
@@ -3,5 +3,5 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install ripgrep cifs-utils samba borgbackup lm-sensors exiftool syncthing neovim htop stress s-tui nload liblzo2-dev git tmux openssh-server tree  fuse libfuse-dev pkg-config -y
+sudo apt install ripgrep cifs-utils samba borgbackup lm-sensors exiftool syncthing neovim htop stress s-tui nload liblzo2-dev git tmux openssh-server tree  fuse libfuse-dev pkg-config sshfs -y
 
