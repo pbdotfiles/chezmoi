575c782eaede42d636cd4c95d7001ff9715521ff
pb4git <pb.pubpub@gmail.com>
2024-11-08 21:20:46 +0100

    various fixes when installing ser

--- bitwarden.sh
+++ bitwarden.sh
@@ -3,7 +3,8 @@
 curl -L "https://vault.bitwarden.com/download/?app=cli&platform=linux" > bw.zip
 unzip bw.zip
 chmod +x bw
-sudo mv bw ~/bin/
+mv bw ~/bin/
 rm bw.zip
 ~/bin/bw config server https://vault.bitwarden.eu
-export BW_SESSION=$(bw login --raw)
+export PATH="$HOME/bin:$PATH"
+export BW_SESSION=$($HOME/bin/bw login --raw)
--- run_onchange_20_mint_setup.sh
+++ run_onchange_20_mint_setup.sh
@@ -33,13 +33,13 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
 sudo apt update
-# The line above installs wine's latest staging version. It doesn't work with Trackmania.
-# sudo apt install --yes --install-recommends winehq-staging # FIXEDVERSION
+# The line below installs wine's latest staging version. It doesn't work with Trackmania.
+sudo apt install --yes --install-recommends winehq-staging # FIXEDVERSION
 # Instead we force install the latest version known to work with Trackmania:
-sudo apt install --install-recommends wine-staging-i386=9.17~noble-1 -y
-sudo apt install --install-recommends wine-staging-amd64=9.17~noble-1 -y
-sudo apt install --install-recommends wine-staging=9.17~noble-1 -y
-sudo apt install --install-recommends winehq-staging=9.17~noble-1 -y
+# sudo apt install --install-recommends wine-staging-i386=9.17~noble-1 -y
+# sudo apt install --install-recommends wine-staging-amd64=9.17~noble-1 -y
+# sudo apt install --install-recommends wine-staging=9.17~noble-1 -y
+# sudo apt install --install-recommends winehq-staging=9.17~noble-1 -y
 
 sudo apt install winetricks -y
 winetricks dxvk
