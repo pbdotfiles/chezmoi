6b8df605ba40978b782c9bb79f2aaa67e7eff081
pb4git <pb.pubpub@gmail.com>
2025-12-11 00:38:54 +0100

    add -y flag for Brave install

--- cm/.chezmoiscripts/run_onchange_28_setup_brave.sh
+++ cm/.chezmoiscripts/run_onchange_28_setup_brave.sh
@@ -7,7 +7,7 @@ if ! command -v brave-browser &>/dev/null; then
   sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
   sudo apt update
-  sudo apt install brave-browser
+  sudo apt install brave-browser -y
   echo "Finished installing Brave Browser."
 else
   echo "Brave Browser is already installed."
