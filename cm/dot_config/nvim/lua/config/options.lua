4dd13e314c2d3f06dc76ef0e761e3c093b3a4b33
pb4git <pb.pubpub@gmail.com>
2025-12-11 23:19:20 +0100

    enable mini-files by default in nvim

---
+++ cm/dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- The setting below is necessary so that we can activate mini.files in ~/.config/nvim/lua/plugins/extras.lua
+vim.g.lazyvim_check_order = false
