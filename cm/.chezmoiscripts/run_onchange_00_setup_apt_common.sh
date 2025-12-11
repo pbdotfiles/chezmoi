fc70d910ca07893a1c0d11a797c717677d9e2a37
pb4git <pb.pubpub@gmail.com>
2025-12-11 19:51:35 +0100

    add jq to apt install

--- cm/.chezmoiscripts/run_onchange_00_setup_apt_common.sh
+++ cm/.chezmoiscripts/run_onchange_00_setup_apt_common.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install ripgrep cifs-utils samba borgbackup lm-sensors exiftool syncthing htop btop stress s-tui nload liblzo2-dev git tmux openssh-server tree fuse3 libfuse-dev pkg-config sshfs bat -y
+sudo apt install ripgrep cifs-utils samba borgbackup lm-sensors exiftool syncthing htop btop stress s-tui nload liblzo2-dev git tmux openssh-server tree fuse3 libfuse-dev pkg-config sshfs bat jq -y
