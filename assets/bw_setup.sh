774b8c0d98fb81547b492b35079d7d6931d81af7
pb4git <pb.pubpub@gmail.com>
2025-12-12 13:13:34 +0100

    add unzip installation at the beginning of bw_setup if it's not already present on the system

--- assets/bw_setup.sh
+++ assets/bw_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -euo pipefail
+
+# Check we have the "unzip" prerequisite
+if ! command -v unzip &>/dev/null; then
+  sudo apt install -y unzip
+fi
+
 # Setup bitwarden on this computer
 curl -L "https://vault.bitwarden.com/download/?app=cli&platform=linux" >bw.zip
 unzip bw.zip
