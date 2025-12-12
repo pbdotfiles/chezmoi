7b849e6868456dbd731bb3e9757492de3019947d
pb4git <pb.pubpub@gmail.com>
2025-12-12 12:21:38 +0100

    Switch from FiraCode Nerd Font to JetBrainsMono Nerd Font

--- cm/.chezmoiscripts/run_onchange_90_install_fonts.sh
+++ cm/.chezmoiscripts/run_onchange_90_install_fonts.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 #################################################
-# Download latest version of the FiraCode fonts
+# Download latest version of the JetBrainsMono fonts
 #################################################
 
 fonts_dir="${HOME}/.local/share/fonts"
@@ -11,14 +11,14 @@ if [ ! -d "${fonts_dir}" ]; then
   mkdir -p "${fonts_dir}"
 fi
 
-URL=$(wget -q -O - https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*FiraCode.zip' | awk -F'"' '{print $4}')
+URL=$(wget -q -O - https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | grep 'browser_download_url.*JetBrainsMono.zip' | awk -F'"' '{print $4}')
 
 if [ -z "$URL" ]; then
-  echo "Error: Could not find FiraCode.zip in the latest Nerd Fonts release."
+  echo "Error: Could not find JetBrainsMono.zip in the latest Nerd Fonts release."
   exit 1
 fi
 
-echo "Downloading latest FiraCode version from: $URL"
+echo "Downloading latest JetBrainsMono version from: $URL"
 wget -q -O font.zip "$URL"
 unzip -o -q -d "${fonts_dir}" font.zip
 rm font.zip
