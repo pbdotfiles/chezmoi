edcb6a3baa2b2dc1933dc4b544920a31061e7095
pb4git <pb.pubpub@gmail.com>
2025-12-13 21:55:29 +0100

    Various changes in default packages setup in apt common.

--- cm/.chezmoiscripts/run_onchange_00_setup_apt_common.sh
+++ cm/.chezmoiscripts/run_onchange_00_setup_apt_common.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install ripgrep cifs-utils samba borgbackup lm-sensors exiftool syncthing htop btop stress s-tui nload liblzo2-dev git tmux openssh-server tree fuse3 libfuse-dev pkg-config sshfs bat jq sshpass -y
+sudo apt install cifs-utils borgbackup lm-sensors exiftool syncthing htop btop stress s-tui nload liblzo2-dev git tmux openssh-server tree fuse3 libfuse-dev pkg-config sshfs bat jq sshpass curl git build-essential ripgrep fd-find unzip xclip python3-venv python3-dev python3-setuptools python3-pynvim imagemagick sqlite3 libsqlite3-dev luarocks zip wget 7zip poppler-utils -y
