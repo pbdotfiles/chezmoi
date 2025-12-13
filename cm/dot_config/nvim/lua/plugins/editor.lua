cc58f4901df5ebca64c804e4962727916065081f
pb4git <pb.pubpub@gmail.com>
2025-12-13 11:40:47 +0100

    add configuration in nvim such that live grep looks into hidden files too

--- cm/dot_config/nvim/lua/plugins/editor.lua
+++ cm/dot_config/nvim/lua/plugins/editor.lua
@@ -1,15 +1,19 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        sources = {
-          files = {
-            hidden = true, -- Enable hidden files
-            ignored = false, -- Keep gitignored files hidden (set to true to show them)
-          },
-        },
-      },
-    },
-  },
+	{
+		"folke/snacks.nvim",
+		opts = {
+			picker = {
+				sources = {
+					files = {
+						hidden = true, -- Enable hidden files
+						ignored = false, -- Keep gitignored files hidden (set to true to show them)
+					},
+					grep = {
+						hidden = true,
+						ignored = false,
+					},
+				},
+			},
+		},
+	},
 }
