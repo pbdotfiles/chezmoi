e855352cf571551b62f35c4f26b03c439c572ddd
pb4git <pb.pubpub@gmail.com>
2026-09-21 11:27:43 +0200

    Fixes post WSL reinstall on work computer

--- cm/dot_config/nvim/lua/plugins/editor.lua
+++ cm/dot_config/nvim/lua/plugins/editor.lua
@@ -25,6 +25,12 @@ return {
 	{
 		"saghen/blink.cmp",
 		optional = true,
+		dependencies = {
+			"saghen/blink.lib",
+		},
+		build = function()
+			require("blink.cmp").build():pwait()
+		end,
 		opts = {
 			keymap = {
 				preset = "default",
